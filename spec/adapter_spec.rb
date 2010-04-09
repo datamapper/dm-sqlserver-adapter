f2feca5bdb4e7a0ba14baf5d951c12c50d2e5876
snusnu <gamsnjaga@gmail.com>
2010-04-09 02:28:10 +0200

    No more need to require an explicit automigration extension
    See http://is.gd/bkM8K

--- spec/adapter_spec.rb
+++ spec/adapter_spec.rb
@@ -1,12 +1,11 @@
 require 'spec_helper'
 
-require 'dm-migrations/adapters/dm-sqlserver-adapter'
-require 'dm-sqlserver-adapter'
+require 'dm-migrations'
 
 require 'dm-core/spec/adapter_shared_spec'
 require 'dm-do-adapter/spec/shared_spec'
 
-describe DataMapper::Adapters::SqlserverAdapter do
+describe 'DataMapper::Adapters::SqlserverAdapter' do
 
   before :all do
     @adapter    = DataMapper.setup(:default, 'sqlserver://dm_core_test:dm_core_test@localhost/dm_core_test;instance=SQLEXPRESS')
